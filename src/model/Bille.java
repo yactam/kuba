c2653076a597b069cb6afb2d8312d8a73d460b73
rouyera <aymericrouyer@gmail.com>
2023-01-29 21:13:15 +0100

    Commenter les erreurs temporaires

--- src/model/Bille.java
+++ src/model/Bille.java
@@ -1,16 +1,16 @@
-package model;
+//package model;
 
 public class Bille {
-    private Position pos;
+    //private Position pos;
     private Couleur color;
 
-    public Bille(Couleur c, Position p){
+    public Bille(Couleur c/*, Position p*/){
         color = c;
-        pos = p;
+        //pos = p;
     }
 
     public Couleur getColor() { return color; }
-    public Position getPos() { return pos; }
+    //public Position getPos() { return pos; }
 
 
 }
