1fd4869f7c833520fece7108e248a2b7cd064f14
TAMDRARI Yacine <yacinetamdrari5@gmail.com>
2023-02-02 11:04:32 +0100

    bille et couleur

--- src/model/Bille.java
+++ src/model/Bille.java
@@ -1,16 +1,18 @@
-//package model;
+package model;
+
+import javax.swing.text.Position;
 
 public class Bille {
-    //private Position pos;
-    private Couleur color;
+    private Position pos;
+    private final Couleur color;
 
-    public Bille(Couleur c/*, Position p*/){
+    public Bille(Couleur c, Position p){
         color = c;
-        //pos = p;
+        pos = p;
     }
 
     public Couleur getColor() { return color; }
-    //public Position getPos() { return pos; }
+    public Position getPos() { return pos; }
 
 
 }
