da335de555db8f1334a1e6c3bf483752a0ba3b50
YRR0 <mendritianararison@gmail.com>
2023-02-02 18:38:37 +0100

    Name change and addition of Next and nextDir

--- src/model/mouvement/Position.java
+++ src/model/mouvement/Position.java
@@ -1,18 +1,34 @@
 package model.mouvement;
 
 public class Position {
-	private final int x;
-	private final int y;
-	
-	public Position(int x, int y) {
-		this.x = x; 
-		this.y = y;
+	private final int i;
+	private final int j;
+
+	public Position(int i, int j) {
+		this.i = i;
+		this.j = j;
 	}
-	
-	public int gPosX() {
-		return this.x;
+
+	public int gPosI() {
+		return this.i;
 	}
-	public int gPosY() {
-		return this.y;
+
+	public int gPosJ() {
+		return this.j;
 	}
-}	
+
+	public Position next(Direction d) {
+		return new Position(this.i + d.gDirI(), this.j + d.gDirJ());
+	}
+
+	public Direction nextDir(Position a, Position b) {
+		if (b.i < a.i)
+			return Direction.NORD;
+		else if (b.i > a.i)
+			return Direction.SUD;
+		else if (b.j < a.j)
+			return Direction.OUEST;
+		else
+			return Direction.EST;
+	}
+}
