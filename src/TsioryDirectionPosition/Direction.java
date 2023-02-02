1bd1a09845252932d35337dacefb252ce3e2ac40
TAMDRARI Yacine <yacinetamdrari5@gmail.com>
2023-02-02 10:55:56 +0100

    direction et position

--- src/TsioryDirectionPosition/Direction.java
+++ src/TsioryDirectionPosition/Direction.java
@@ -4,8 +4,8 @@ public enum Direction {
 	// Placement dans un tableau de tableau 
 	NORD(-1,0),SUD(1,0),OUEST(0,-1),EST(0,1);
 	
-	private int x;
-	private int y;
+	private final int x;
+	private final int y;
 	
 	Direction(int x, int y){
 		this.x = x;
--- src/TsioryDirectionPosition/Position.java
+++ src/TsioryDirectionPosition/Position.java
@@ -1,8 +1,8 @@
 package TsioryDirectionPosition;
 
 public class Position {
-	private int x;
-	private int y;
+	private final int x;
+	private final int y;
 	
 	public Position(int x, int y) {
 		this.x = x; 
