2105bf19d4476b6e12fa4bd5245f268cb7bc9cf2
TAMDRARI Yacine <yacinetamdrari5@gmail.com>
2023-02-15 19:44:21 +0100

    add image and delete position

--- src/model/Bille.java
+++ src/model/Bille.java
@@ -1,18 +1,48 @@
 package model;
 
-import model.mouvement.*;;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Objects;
 
 public class Bille {
-    private Position pos;
     private final Couleur color;
+    public static final int width = 50;
+    private final BufferedImage image;
 
-    public Bille(Couleur c, Position p){
+    public Bille(Couleur c){
         color = c;
-        pos = p;
+        switch (color) {
+            case NOIR -> {
+                try {
+                    image = ImageIO.read(Objects.requireNonNull(getClass().getResource("/resources/black.png")));
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            case BLANC -> {
+                try {
+                    image = ImageIO.read(Objects.requireNonNull(getClass().getResource("/resources/white.png")));
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            case ROUGE -> {
+                try {
+                    image = ImageIO.read(Objects.requireNonNull(getClass().getResource("/resources/red.png")));
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            default -> image = null;
+        }
     }
 
     public Couleur getColor() { return color; }
-    public Position getPos() { return pos; }
+
+    public BufferedImage image() {
+        return image;
+    }
 
     @Override
     public String toString() {
