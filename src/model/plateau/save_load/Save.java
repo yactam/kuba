6701a33b4b1e4b03bb0bf6fe0bc8c64970235cf4
NAFAA Emma <nafaaemma@MacBook-Air-de-NAFAA.local>
2023-02-23 03:33:44 +0100

    add save class

--- src/model/plateau/save_load/Save.java
+++ src/model/plateau/save_load/Save.java
@@ -1,5 +1,23 @@
 package model.plateau.save_load;
 
-public class Save {
-    
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+
+import model.plateau.Board;
+
+public abstract class Save {
+    public static void save(Board board){
+        try {
+            FileOutputStream fileOutputStream 
+                = new FileOutputStream("yourfile.txt");
+            ObjectOutputStream objectOutputStream 
+                = new ObjectOutputStream(fileOutputStream);
+            objectOutputStream.writeObject(board);
+            objectOutputStream.flush();
+            objectOutputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
