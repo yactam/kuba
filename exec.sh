8b74c941574ca7175c4138c4388283f551d6a8b8
TAMDRARI Yacine <yacinetamdrari5@gmail.com>
2023-03-03 17:30:13 +0100

    fix exec.sh script

--- exec.sh
+++ exec.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-javac -classpath src/ src/Main.java
-java -classpath src/ Main
+javac -classpath src/ src/com/kuba/Main.java
+java -classpath src/ com.kuba.Main
 find . -type f -path "./src/*" -name "*.class" -delete
 
