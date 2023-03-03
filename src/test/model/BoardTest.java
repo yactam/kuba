8c6ab7dde0027813267fcf6a8eb19bd6d5a8c84c
TAMDRARI Yacine <yacinetamdrari5@gmail.com>
2023-03-03 17:01:22 +0100

    build some tests white JUnit

--- src/test/model/BoardTest.java
+++ src/test/model/BoardTest.java
@@ -1,6 +1,22 @@
 package test.model;
 
+import com.kuba.model.plateau.Board;
+import com.kuba.model.plateau.Couleur;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class BoardTest {
-  
+
+    @Test
+    public void initialState() {
+        Board board = new Board(2);
+        board.initBoard();
+        assertEquals(board.currentPlayer(), Couleur.BLANC);
+        assertFalse(board.gameOver());
+        assertEquals(board.getAllPossibleMoves(Couleur.BLANC, Couleur.NOIR).size(), 16);
+        assertEquals(board.getAllPossibleMoves(Couleur.BLANC).size(), 8);
+        assertEquals(board.getAllPossibleMoves(Couleur.NOIR).size(), 8);
+    }
+
 }
--- src/test/model/MiniMaxTest.java
+++ src/test/model/MiniMaxTest.java
@@ -1,6 +1,35 @@
 package test.model;
 
+import com.kuba.model.mouvement.Direction;
+import com.kuba.model.mouvement.Mouvement;
+import com.kuba.model.mouvement.Position;
+import com.kuba.model.plateau.Bille;
+import com.kuba.model.plateau.Board;
+import com.kuba.model.plateau.Couleur;
+import com.kuba.model.player.ai.MiniMax;
+import com.kuba.model.player.ai.StandardBoardEvaluator;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class MiniMaxTest {
-  
+
+    @Test
+    public void testCase1() {
+        Board board = new Board(2);
+        for(int i = 0; i < board.size(); i++) {
+            for(int j = 0; j < board.size(); j++) {
+                board.initCell(i, j);
+            }
+        }
+        board.board(0, 6).setBille(new Bille(Couleur.NOIR));
+        board.board(1, 6).setBille(new Bille(Couleur.ROUGE));
+        board.board(2, 6).setBille(new Bille(Couleur.ROUGE));
+        board.board(3, 6).setBille(new Bille(Couleur.BLANC));
+
+        MiniMax miniMax = new MiniMax(4);
+        Mouvement mouvement = miniMax.execute(board, Couleur.BLANC);
+        assertEquals(mouvement, new Mouvement(new Position(3, 6), Direction.NORD));
+    }
+
 }
