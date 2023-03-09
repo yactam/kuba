72db024a4a8fee4c70e05eee9515f9b4dd6845d7
Ibrahime <ibrahime_cmr@outlook.com>
2023-03-09 10:32:38 +0100

    update dans player

--- src/view/PlayerView.java
+++ src/view/PlayerView.java
@@ -16,14 +16,14 @@ public class PlayerView extends JPanel {
         Color bg = new Color(70*(count%2+1)+50,70*(count%2+1)+50,70*(count%2+1)+50);
         this.setBackground(bg);
         this.setLayout(new GridLayout(3,1));
-        this.setPreferredSize(new Dimension(250, 340));
+        this.setPreferredSize(new Dimension(300, 340));
         joueur = j;
-        nom = new JLabel("   "+j.getNom());
+        nom = new JLabel("     "+j.getNom());
         nom.setFont(new Font("Serif", Font.BOLD, 18));
-        JLabel lab1 = new JLabel("  Billes restantes");
-        JLabel lab2 = new JLabel("  Billes rouges capturées");
-        billesRestantes = new JLabel(j.getNbAdversaireCapture()+"  ");
-        billesCapturesR = new JLabel(j.getNbBilleRougeCapturer()+"  ");
+        JLabel lab1 = new JLabel("   Billes restantes");
+        JLabel lab2 = new JLabel("   Billes rouges capturées");
+        billesRestantes = new JLabel(j.getNbAdversaireCapture()+"   ");
+        billesCapturesR = new JLabel(j.getNbBilleRougeCapturer()+"   ");
         this.add(nom);
         JPanel rest = new JPanel(new BorderLayout());
         rest.add(lab1, BorderLayout.WEST);
@@ -38,8 +38,8 @@ public class PlayerView extends JPanel {
     }
 
     public void update(){
-        billesRestantes = new JLabel(joueur.getNbAdversaireCapture()+"  ");
-        billesCapturesR = new JLabel(joueur.getNbBilleRougeCapturer()+"  ");
+        billesRestantes = new JLabel(joueur.getNbAdversaireCapture()+"   ");
+        billesCapturesR = new JLabel(joueur.getNbBilleRougeCapturer()+"   ");
         this.repaint();
     }
 }
