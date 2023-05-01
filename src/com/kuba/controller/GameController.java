e5ce28c5f13f4c77fdbf1e815171be091bbf236f
TAMDRARI Yacine <yacinetamdrari5@gmail.com>
2023-05-01 21:37:55 +0200

    fix the controller for all types of controllers ? :)

--- src/com/kuba/controller/GameController.java
+++ src/com/kuba/controller/GameController.java
@@ -1,5 +1,6 @@
 package com.kuba.controller;
 
+import com.kuba.Game;
 import com.kuba.model.mouvement.Direction;
 import com.kuba.model.mouvement.Mouvement;
 import com.kuba.model.mouvement.MoveStatus;
@@ -10,6 +11,7 @@ import com.kuba.model.player.Joueur;
 import com.kuba.model.player.ai.IA;
 import com.kuba.vue.BilleAnimateView;
 import com.kuba.vue.BoardView;
+import com.kuba.vue.GameView;
 
 import javax.swing.*;
 import java.awt.*;
@@ -29,37 +31,72 @@ public class GameController {
     private Position from;
     private Direction direction;
     private final Son son;
+    private final GameView gameView;
     private final BoardView boardView;
 
-    public GameController(Board board, BoardView boardView, Joueur blanc, Joueur noir) {
-        this.boardView = boardView;
+    public GameController(Board board, GameView gameView, Joueur blanc, Joueur noir) {
+        this.gameView = gameView;
+        this.boardView = gameView.boardview();
         this.board = board;
         this.blanc = blanc;
         this.noir = noir;
         this.courant = blanc;
         son = new Son();
         son.playMusic(0);
+        initListeners();
+
+    }
+
+    private void initListeners() {
         ((JPanel)board.getObserver()).addMouseListener(new MouseController());
         ((JPanel)board.getObserver()).addKeyListener(new KeyController());
-        ((JPanel)board.getObserver()).setFocusable(true);
-        ((JPanel)board.getObserver()).requestFocusInWindow();
+        enableController(true);
+        gameView.recommencer(e -> {
+            blanc.setNbAdversaireCapturee(0);
+            blanc.setNbRougesCapturee(0);
+            noir.setNbAdversaireCapturee(0);
+            noir.setNbRougesCapturee(0);
+            new Game((board.size()+1) / 4, blanc, noir);
+        });
+
+        gameView.mute(e -> {
+            if(son.isPlaying()) {
+                son.stopMusic();
+            } else {
+                son.playMusic(0);
+            }
+            enableController(true);
+        });
+
+        gameView.abandonner(e -> {
+            enableController(false);
+            gameView.showError((courant == blanc ? noir.getNom() : blanc.getNom()) + " a gagné la partie.");
+        });
     }
 
     private void deplacement(Direction d){
+        gameView.cleanError();
         try{
             if(d !=null && from != null){
                 MoveStatus moveStatus = courant.move(board, new Mouvement(from, d));
                 if(moveStatus.getStatus() == MoveStatus.Status.BASIC_MOVE){
                     lancerAnimationBille();
                     son.playSoundEffect(1);
+                    from = from.next(d);
                     changePlayer();
                 }
                 else if(moveStatus.getStatus() == MoveStatus.Status.MOVE_OUT){
                     lancerAnimationBille();
                     son.playSoundEffect(1);
+                    from = from.next(d);
+                    gameView.updateScore();
                 } else {
-                    System.out.println(moveStatus.getMessage());
+                    gameView.showError(moveStatus.getMessage());
                     son.playSoundEffect(2);
+                }
+                if(board.gameOver(blanc, noir)) {
+                    gameView.showError(board.getWinner(blanc, noir).getNom() + " a gagné la partie.");
+                    enableController(false);
                 }
             }
         }
@@ -87,24 +124,36 @@ public class GameController {
     }
 
     public void changePlayer(){
+        gameView.cleanError();
         if(courant == blanc){
             courant = noir;
+            gameView.changePlayer(2);
         }else{
             courant = blanc;
+            gameView.changePlayer(1);
         }
         if(courant instanceof IA) {
-            boardView.setFocusable(false);
+            enableController(false);
             aiPlayer();
         } else {
-            boardView.setFocusable(true);
-            boardView.requestFocusInWindow();
+            enableController(true);
         }
+    }
+
+    public void enableController(boolean enable) {
+        boardView.setFocusable(enable);
+        if(enable) boardView.requestFocusInWindow();
     }
 
     private void aiPlayer() {
         IA aiPlayer = (IA) courant;
         Mouvement mouvement = aiPlayer.getMouvement(board);
         MoveStatus moveStatus = aiPlayer.move(board, mouvement);
+        gameView.updateScore();
+        if(board.gameOver(blanc, noir)) {
+            gameView.showError(board.getWinner(blanc, noir).getNom() + " a gagné la partie.");
+            return;
+        }
         if(moveStatus.getStatus() == MoveStatus.Status.BASIC_MOVE) changePlayer();
         else aiPlayer();
     }
@@ -155,17 +204,9 @@ public class GameController {
                 if(from != null){
                     assert to != null;
                     direction = from.nextDir(to);
-                    System.out.println(direction);
                     try{
                         if(direction != null && from != null){
-                            MoveStatus moveStatus = courant.move(board, to, direction);
-                            if(moveStatus.getStatus() == MoveStatus.Status.BASIC_MOVE) {
-                                changePlayer();
-                                son.playSoundEffect(1);
-                                lancerAnimationBille();
-                            } else if(!moveStatus.isLegal()) {
-                                System.out.println(moveStatus.getMessage());
-                            }
+                            deplacement(direction);
                         }
                     }
                     catch(Exception exception){
