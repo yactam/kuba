20ce8e062c9c713d0fc6cd67c6878f05aa4f1249
Tsiory RARISON (YRRO) <113048356+YRR0@users.noreply.github.com>
2023-05-04 11:33:44 +0200

    added a simple menu  to start an online game

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 import com.kuba.Game;
+import com.kuba.GameOnline;
 
 import javax.swing.*;
 import com.kuba.reseau.*;
 public class Main {
     public static void main(String[] args) {
-        //new Game();
-        new Online();
+        new Game();
+        //new GameOnline();
     }
 }
---
+++ src/com/kuba/GameOnline.java
@@ -0,0 +1,55 @@
+package com.kuba;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import com.kuba.reseau.*;
+
+public class GameOnline extends JFrame {
+    private JButton bouton;
+
+    public GameOnline() {
+        super("Online");
+        setContentPane(new ColorPanel());
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        int x = (dim.width - bouton.getSize().width) / 2;
+        int y = (dim.height - bouton.getSize().height) / 2;
+        bouton.setLocation(x, y);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setSize(600, 400); 
+        setVisible(true);
+        new Controller();
+    }
+
+    private class Controller extends MouseAdapter {
+        public Controller() {
+            bouton.addActionListener(e -> {
+               new Online();
+               dispose();
+            });
+        }
+    }
+
+    private class ColorPanel extends JPanel {
+        public ColorPanel() {
+            setBackground(new Color(119, 81, 44));
+    
+            ImageIcon icon = new ImageIcon("src/resources/onllineButton.png"); 
+            bouton = new JButton(icon);
+            bouton.setPreferredSize(new Dimension(icon.getIconWidth(), icon.getIconHeight()));
+            bouton.setOpaque(false);
+            bouton.setContentAreaFilled(false);
+            bouton.setBorderPainted(false);
+            add(bouton);
+    
+            setLayout(new GridBagLayout());
+            add(bouton, new GridBagConstraints());
+            bouton.setAlignmentX(Component.CENTER_ALIGNMENT);
+            bouton.setAlignmentY(Component.CENTER_ALIGNMENT);
+        }
+    
+    }
+
+}
