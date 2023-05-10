9f410e3a9d838d79db7c07704ae2a97feabe393c
TAMDRARI Yacine <yacinetamdrari5@gmail.com>
2023-05-11 01:58:13 +0200

    thread safe

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
+import javax.swing.SwingUtilities;
+
 import com.kuba.Game;
 import com.kuba.GameOnline;
 
 public class Main {
     public static void main(String[] args) {
-        new Game();
+        SwingUtilities.invokeLater(
+            () -> {
+                new Game();
+            }
+        );
+        
     }
 }
